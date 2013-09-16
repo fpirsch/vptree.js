9d740c648ed03db152a1f2a345b7a538c6927ea3
fpirsch <fpirsch@free.fr>
2013-09-16 20:10:31 +0200

    Create intro.js

---
+++ src/intro.js
@@ -0,0 +1,25 @@
+/*╔═════════════════════════════════════════════════════════════════════════════════════════════════════════╗
+ *║                                                                                                         ║
+ *║      vptree.js                                                                                          ║
+ *║      A javascript implementation of the Vantage-Point Tree algorithm                                    ║
+ *║                                                                                                         ║
+ *╚═════════════════════════════════════════════════════════════════════════════════════════════════════════╝
+ */
+
+"use strict";
+
+//https://github.com/umdjs/umd/blob/master/commonjsStrictGlobal.js
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['exports'], function (exports) {
+            factory(root.VPTreeFactory = exports);
+        });
+    } else if (typeof exports === 'object') {
+        // CommonJS
+        factory(exports);
+    } else {
+        // Browser globals
+        factory(root.VPTreeFactory = {});
+    }
+}(this, function (exports) {
