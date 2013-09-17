72b775b8e88e91612d0a6f5fa676450d48c9a138
fpirsch <fpirsch@free.fr>
2013-09-17 14:53:56 +0200

    Create header.js

---
+++ src/header.js
@@ -0,0 +1,12 @@
+/*╔═════════════════════════════════════════════════════════════════════════════════════════════════════════╗
+ *║                                                                                                         ║
+ *║      vptree.js v{{version}}                                                                             ║
+ *║      https://github.com/fpirsch/vptree.js                                                               ║
+ *║                                                                                                         ║
+ *║      A javascript implementation of the Vantage-Point Tree algorithm                                    ║
+ *║      ISC license (http://opensource.org/licenses/ISC). François Pirsch. 2013.                           ║
+ *║                                                                                                         ║
+ *║      Date: {{    date     }}                                                                            ║
+ *║                                                                                                         ║
+ *╚═════════════════════════════════════════════════════════════════════════════════════════════════════════╝
+ */
